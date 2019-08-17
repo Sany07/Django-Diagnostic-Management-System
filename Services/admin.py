42fb1dd07023de052aa53a61991b1aef50011a49
Sany <sany.baust@gmail.com>
2019-08-17 19:21:34 +0600

    Services App Uptated

---
+++ Services/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Service,Vision,Mission,Center
+
+
+admin.site.register(Service)
+admin.site.register(Vision)
+admin.site.register(Mission)
+admin.site.register(Center)
