fdb99ba4b6ec84196850d49da23ee5a4d7707c55
Sany <sany.baust@gmail.com>
2019-08-17 18:01:03 +0600

    Remove duplicated directory

--- Services/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Service,Vision,Mission,Center
-
-
-admin.site.register(Service)
-admin.site.register(Vision)
-admin.site.register(Mission)
-admin.site.register(Center)
